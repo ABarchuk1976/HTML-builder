4fb28bb3a9585234a73d8299053858728631eeb6
Barchuk Andrii <abarchuk1976@gmail.com>
2023-05-06 22:32:43 +0300

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fsAsync = require('fs').promises;
+const path = require('path');
 
+const currentPath = path.join(__dirname, './files');
+const copyPath = path.join(__dirname, './files-copy');
+
+const copyDir = async (path, copyPath) => {
+  await fsAsync.mkdir(copyPath, { recursive: true });
+
+  const files = await fsAsync.readdir(path);
+
+  files.forEach(async (file) => {
+    const filePath = path + '/' + file;
+    const fileCopyPath = copyPath + '/' + file;
+
+    const stats = await fsAsync.stat(filePath);
+
+    if (stats.isFile()) {
+      const data = await fsAsync.readFile(filePath);
+
+      await fsAsync.writeFile(fileCopyPath, data);
+    } else {
+      await copyDir(filePath, fileCopyPath);
+    }
+  });
+};
+
+copyDir(currentPath, copyPath);
