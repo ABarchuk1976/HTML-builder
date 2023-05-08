0c73e7f59ffc4d12005919b54329318a2367a3d0
Barchuk Andrii <abarchuk1976@gmail.com>
2023-05-08 13:25:51 +0300

    refactor: index.js at 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,14 +3,14 @@ const fsAsync = require('fs').promises;
 
 const TASK_FILE = 'text.txt';
 const filePath = path.join(__dirname, `${TASK_FILE}`);
-const text = [];
+let text = '';
 
 const readStream = async (filePath) => {
   const file = await fsAsync.open(filePath);
   const stream = file.createReadStream({ encoding: 'utf8' });
 
-  stream.on('data', (part) => text.push(part));
-  stream.on('end', () => console.log(text.join('')));
+  stream.on('data', (chunk) => (text += chunk));
+  stream.on('end', () => console.log(text));
 };
 
 readStream(filePath);
